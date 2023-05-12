7a5d572861b5e4750c22674050042f48cde7c16c
SirBaum <SirBaum@protonmail.com>
2023-05-12 16:38:53 +0200

    added comments and annotation

--- examples/training/validate.py
+++ examples/training/validate.py
@@ -1,6 +1,31 @@
-def validate(valid_loader, model, criterion, device):
+from __future__ import annotations
+
+# Annotations
+__author__ = "Sebastian Baum"
+__maintainer__ = "Sebastian Baum"
+__version__ = "1.0.0"
+__status__ = "Prototype"
+
+# For annotation
+from typing import Tuple
+
+# Imports
+import torch
+from torch.utils.data import DataLoader
+
+
+def validate(
+    valid_loader: DataLoader, model: torch.nn.Module, criterion, device: torch.device
+) -> Tuple(torch.nn.Module, float):
     """
     Function for the validation step of the training loop
+
+    :param valid_loader: dataloader with validation data
+    :param model: the model that will be validated
+    :param criterion: the criterion to calculate the loss
+    :param device: the device to load the data
+
+    :param return: the model and the epoch loss
     """
     model.eval()
     running_loss = 0
